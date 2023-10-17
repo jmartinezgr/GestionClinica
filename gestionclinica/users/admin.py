12d91cecaa9ea6543938e944c4aa01607fa58d66
Juan <josemargri3@gmail.com>
2023-10-16 19:19:42 -0500

    Se elimina el proyecto de prueba

--- gestionclinica/users/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser,Role  # Asegúrate de importar tu modelo
-
-admin.site.register(CustomUser)  # Registra el modelo en la interfaz de administración
-admin.site.register(Role)
